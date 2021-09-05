f1e33ea4c4b7df782eb1e3e95a0352faf2d6b83a
IbrahimBHMBS <bb.med2000@gmail.com>
2021-09-05 21:02:36 +0100

    Feat: Setting up the project structure and added a Navbar Component

---
+++ components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function NavBar() {
+    return (
+        <nav className="flex justify-between">
+            <p className="text-2xl font-bold text-gray-800">My ToDos</p>
+            <div className="flex">
+                <a href="/api/logout" 
+                className="my-2 px-4 py-2 border-2 border-blue-500 rounded-md bg-gradient-to-b from-blue-600 to-blue-400 text-white shadow-lg hover:bg-red-600 transform hover:bg-blue-600 transition duration-500 ease-in-out hover:scale-125"
+                >Logout</a>
+                <a href="/api/login" 
+                className="my-2 px-4 py-2 border-2 border-blue-500 rounded-md bg-gradient-to-b from-blue-600 to-blue-400 text-white shadow-lg hover:bg-red-600 transform hover:bg-blue-600 transition duration-500 ease-in-out hover:scale-125"
+                >Login</a>
+            </div>
+        </nav>
+    )
+}
---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/index.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+  <div className="container mx-auto my-10">
+    <Component {...pageProps} />
+  </div>
+  )
+}
+
+export default MyApp
